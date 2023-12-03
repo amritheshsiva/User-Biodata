6ab316481e8e4656274ba7c90d91de50b0e4a09e
Amrithesh S <94913836+amritheshsiva@users.noreply.github.com>
2023-12-03 22:54:06 +0530

    Update data.dart

--- data.dart
+++ data.dart
@@ -2,17 +2,17 @@ import 'dart:io';
 void main()
 {
     stdout.write("Enter your name :");
-    String name = stdin.readLineSync()!;
+    String name = stdin.readLineSync();
     stdout.write("Enter your phone number :");
-    String num = stdin.readLineSync()!;
+    String num = stdin.readLineSync();
     stdout.write("Enter your age :");
-    String age = stdin.readLineSync()!;
+    String age = stdin.readLineSync();
     stdout.write("Enter your Height :");
-    String height = stdin.readLineSync()!;
+    String height = stdin.readLineSync();
     stdout.write("Enter your weight :");
-    String weight = stdin.readLineSync()!;
+    String weight = stdin.readLineSync();
     stdout.write("Enter your hobbies :");
-    String hobby = stdin.readLineSync()!;
+    String hobby = stdin.readLineSync();
     print("");
     print("-----------------");
     print("     BIODATA     ");
